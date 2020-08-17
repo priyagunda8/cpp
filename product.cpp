27f6d2539216efe05a0152851ddd7cf63a7a8992
priyagunda8 <69363026+priyagunda8@users.noreply.github.com>
2020-08-17 12:16:15 +0530

    cpp project

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+    int main()
+    {
+        float a,b,product;
+            cout<<"enter a"<<endl;
+            cin>>a;
+            cout<<"enter b"<<endl;
+            cin>>b;
+            product=a*b;
+            cout<<"product of a and b is "<<product<<endl;
+            return 0;
+    }
---
+++ size.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    char c;
+    float f;
+    double d;
+    cout<<"size of char: "<<sizeof (c)<< " byte" <<endl;
+    cout<<" size of int: "<<sizeof (a)<<" bytes"<<endl;
+    cout<<"size of float: "<<sizeof (f)<<" bytes"<<endl;
+    cout<<"size of double: "<<sizeof (d)<<" bytes"<<endl;
+    return 0;
+}
